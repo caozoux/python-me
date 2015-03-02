83a10694144f44bdfd0b8a5f7d9573419ec0d613
czou <cao.zou@windriver.com>
2015-03-02 10:17:22 +0800

    patch.py add cach code

--- module/patch_diff/patch.py
+++ module/patch_diff/patch.py
@@ -21,12 +21,28 @@ def get_git_lot(path, save_name = "gitlog"):
         return
     return
 
+def format_patch(s_dir, commit, number, out_dir=""):
+    "function: run the command: git format-patch \
+     s_dir:   git dir \
+     commit:  git commit id \
+     number:  git format-patch --start-number number \
+     out_dir: save path dir"
+
+    return 
+    git_args="format-patch -1 --start-number " + str(number) + " "
+    if out_dir !="":
+        git_args += "-o " + out_dir + " "
+
+    cmd= "cd "+ s_dir +"; "+ "git "+ git_args + commit
+    os.system(cmd)
+    return 
 
 def find_path_diff_for_file(s_dir, d_dir, filename, show_mode = 0, time="", autor=""):
     "show_mode 1: show debug information"
     #print(s_dir, "  ", d_dir)
     #运行的git command
     git_cmd = "git log --pretty=oneline "
+    patch_number = 0
     if os.path.exists(s_dir) and os.path.exists(d_dir):
         s_file= s_dir + "/" + filename;
         d_file= d_dir + "/" + filename;
@@ -45,94 +61,146 @@ def find_path_diff_for_file(s_dir, d_dir, filename, show_mode = 0, time="", auto
                     git_cmd += "--since=" + time + " "
                 if autor != "":
                     git_cmd += "--author=" + autor + " "
-                #cmd1= "cd "+ s_dir +"; "+"git log --pretty=oneline "+ filename + "| cut -b 42-"
-                cmd1= "cd "+ s_dir +"; "+ git_cmd + filename + "| cut -b 42-"
-                s_cont = excule_cmd_pr(cmd1, 0)
 
-                ss_cont=(s_cont.replace("\n","^")).split("^")
+                cmd1= "cd "+ s_dir +"; "+ git_cmd + filename + " | cut -b -40"
+                #print(cmd1)
+                #cmd1= "cd "+ s_dir +"; "+"git log --pretty=oneline "+ filename + "| cut -b -40"
+                #s_cont = excule_cmd_pr(cmd1, 0)
+                for line in os.popen(cmd1).readlines():
+                    #再次检测是否有pick的patch
+                    commit_diff_id = line[:-1]
+                    cmd2= "cd "+ s_dir +"; "+"git show " + commit_diff_id + " | grep Upstream"
+                    cont = os.popen(cmd2).read()
 
+                    #print(commit_diff_id)
+                    #有upstream的patch
+                    if cont != "":
+                        upstream_commit = cont [-43:-2]
+                        # 检测这个patch是否在windriver linux
+                        #cmd2= "cd "+ d_dir +"; "+"git show -s --format=%s " + upstream_commit + " 2>/dev/null"
+                        cmd2= "cd "+ d_dir +"; "+"git show -s --format=%s " + upstream_commit + " 2>/dev/null"
+                        gitshow_cont = os.popen(cmd2).read()
+                        #print(gitshow_cont+ " 5" + gitshow_cont[:5])
 
-                #执行git log --pretty=oneline 去查看目的文件修改记录
-                #print('\033[1;31;40m')
-                #print("~~~~~~~~~~~~~~log commit:"+d_file,)
-                #print('\033[0m')
-                #cmd1= "cd "+ d_dir +"; "+"git log --pretty=oneline "+ filename + "| cut -b 42-"
-                cmd1= "cd "+ d_dir +"; "+ git_cmd + filename + "| cut -b 42-"
-                d_cont = excule_cmd_pr(cmd1, 0)
-
-                dd_cont=(d_cont.replace("\n","^")).split("^")
-
-
-                ss_rec_list = [ 0 for i in range(len(ss_cont))]
-                dd_rec_list = [ 0 for i in range(len(dd_cont))]
-
-                #比较两个文件记录的差异
-                if show_mode == 1:
-                    print('\033[1;31;40m')
-                    print("~~~~~~~~~~~~~~exit patchs~~~~~~~~~~~~~~:")
-                    print(s_file, "  compare:");
-                    print(d_file)
-                    print('\033[0m')
-
-                s_ii = 0
-                for s_i in ss_cont:
-                    d_ii = 0
-                    for d_i in dd_cont:
-                        if len(s_i) == 0 or len(d_i) == 0 or dd_rec_list[d_ii] == 1:
+                        if gitshow_cont[:5] == "":
+                            #print(commit_diff_id + " lost upstream patch," + upstream_commit) 
+                            print(line)
+                            format_patch(s_dir, commit_diff_id, patch_number)
+                            patch_number += 1
                             pass
                         else:
-                            if  s_i == d_i:
-                                if show_mode == 1:
-                                    print(s_ii, s_i, "<===>", len(d_i), d_ii)
-                                dd_rec_list[d_ii] = 1
-                                ss_rec_list[s_ii] = 1
-                                break
-                            else:
-                                pass
-                                #print(len(s_i), s_i, "<--->", len(d_i), d_i)
-                        d_ii += 1
-                    s_ii += 1
+                            #print(line)
+                            #print("lost upstream patch" + line)
+                            #print("patch is exit")
+                            #print("is upstream patch," + cont + upstream_commit) 
+                            #print(commit_diff_id + "upstream patch," + upstream_commit) 
+                            pass
+                    else:
 
-                #
-                if 1:
-                    print('\033[1;31;40m')
-                    print("~~~~~~~~~~~~~~lost patch~~~~~~~~~~~~~~:")
-                    print(s_file)
-                    print('\033[0m')
+                        # 再次检测commit是否在dst中有
+                        cmd2= "cd "+ d_dir +"; "+"git show -s --format=%s " + commit_diff_id + " 2>/dev/null"
+                        gitshow_cont = os.popen(cmd2).read()
 
-                cmd1= "cd "+ s_dir +"; "+"git log --pretty=oneline "+ filename 
-                s_cont = excule_cmd_pr(cmd1, 0)
-                ss_cont=(s_cont.replace("\n","^")).split("^")
+                        #print(gitshow_cont) 
+                        if gitshow_cont[:5] == "":
+                            #print("find lost patch" + line)
+                            print(line)
+                            format_patch(s_dir, commit_diff_id, patch_number)
+                            patch_number += 1
 
-                for i in range(len(ss_rec_list)):
-                    if ss_rec_list[i] == 0:
-                        if len(ss_cont[i]):
+                        else:
+                            #print("patch is exit")
+                            pass
 
-                            #再次检测是否有pick的patch
-                            #print(ss_cont[i])
-                            commit_diff_id = ss_cont[i][:40]
-                            cmd1= "cd "+ s_dir +"; "+"git show " + commit_diff_id + " | grep Upstream"
-                            cont = os.popen(cmd1).read()
-                            if cont != "":
-                                #有upstream的patch
-                                pass
-                            else:
-                                #只打印commit id
-                                #print(ss_cont[i][:40])
-
-                                # 再次检测commit是否在dst中有
-                                cmd1= "cd "+ d_dir +"; "+"git show -s --format=%s " + ss_cont[i][:40] + " 2>/dev/null"
-                                gitshow_cont = os.popen(cmd1).read()
-                                #print(gitshow_cont)
-                                #print(gitshow_cont[:20])
-                                if gitshow_cont[:5] == "":
-                                    cmd1= "cd "+ s_dir +"; "+"git format-patch -1 " + ss_cont[i][:40] 
-                                    print(cmd1)
-                                    #gitshow_cont = os.popen(cmd1).read()
-                                    #print(gitshow_cont)
-                                    pass
-
-                            #print(ss_cont[i][:40])
+                #print(s_cont)
+#                #cmd1= "cd "+ s_dir +"; "+"git log --pretty=oneline "+ filename + "| cut -b 42-"
+#                cmd1= "cd "+ s_dir +"; "+ git_cmd + filename + "| cut -b 42-"
+#                s_cont = excule_cmd_pr(cmd1, 0)
+#
+#                ss_cont=(s_cont.replace("\n","^")).split("^")
+#
+#
+#                #执行git log --pretty=oneline 去查看目的文件修改记录
+#                #print('\033[1;31;40m')
+#                #print("~~~~~~~~~~~~~~log commit:"+d_file,)
+#                #print('\033[0m')
+#                #cmd1= "cd "+ d_dir +"; "+"git log --pretty=oneline "+ filename + "| cut -b 42-"
+#                cmd1= "cd "+ d_dir +"; "+ git_cmd + filename + "| cut -b 42-"
+#                d_cont = excule_cmd_pr(cmd1, 0)
+#
+#                dd_cont=(d_cont.replace("\n","^")).split("^")
+#
+#
+#                ss_rec_list = [ 0 for i in range(len(ss_cont))]
+#                dd_rec_list = [ 0 for i in range(len(dd_cont))]
+#
+#                #比较两个文件记录的差异
+#                if show_mode == 1:
+#                    print('\033[1;31;40m')
+#                    print("~~~~~~~~~~~~~~exit patchs~~~~~~~~~~~~~~:")
+#                    print(s_file, "  compare:");
+#                    print(d_file)
+#                    print('\033[0m')
+#
+#                s_ii = 0
+#                for s_i in ss_cont:
+#                    d_ii = 0
+#                    for d_i in dd_cont:
+#                        if len(s_i) == 0 or len(d_i) == 0 or dd_rec_list[d_ii] == 1:
+#                            pass
+#                        else:
+#                            if  s_i == d_i:
+#                                if show_mode == 1:
+#                                    print(s_ii, s_i, "<===>", len(d_i), d_ii)
+#                                dd_rec_list[d_ii] = 1
+#                                ss_rec_list[s_ii] = 1
+#                                break
+#                            else:
+#                                pass
+#                                #print(len(s_i), s_i, "<--->", len(d_i), d_i)
+#                        d_ii += 1
+#                    s_ii += 1
+#
+#                #
+#                if 1:
+#                    print('\033[1;31;40m')
+#                    print("~~~~~~~~~~~~~~lost patch~~~~~~~~~~~~~~:")
+#                    print(s_file)
+#                    print('\033[0m')
+#
+#                cmd1= "cd "+ s_dir +"; "+"git log --pretty=oneline "+ filename 
+#                s_cont = excule_cmd_pr(cmd1, 0)
+#                ss_cont=(s_cont.replace("\n","^")).split("^")
+#
+#                for i in range(len(ss_rec_list)):
+#                    if ss_rec_list[i] == 0:
+#                        if len(ss_cont[i]):
+#
+#                            #再次检测是否有pick的patch
+#                            #print(ss_cont[i])
+#                            commit_diff_id = ss_cont[i][:40]
+#                            cmd1= "cd "+ s_dir +"; "+"git show " + commit_diff_id + " | grep Upstream"
+#                            cont = os.popen(cmd1).read()
+#                            if cont != "":
+#                                #有upstream的patch
+#                                pass
+#                            else:
+#                                #只打印commit id
+#                                #print(ss_cont[i][:40])
+#
+#                                # 再次检测commit是否在dst中有
+#                                cmd1= "cd "+ d_dir +"; "+"git show -s --format=%s " + ss_cont[i][:40] + " 2>/dev/null"
+#                                gitshow_cont = os.popen(cmd1).read()
+#                                #print(gitshow_cont)
+#                                #print(gitshow_cont[:20])
+#                                if gitshow_cont[:5] == "":
+#                                    cmd1= "cd "+ s_dir +"; "+"git format-patch -1 " + ss_cont[i][:40] 
+#                                    print(cmd1)
+#                                    #gitshow_cont = os.popen(cmd1).read()
+#                                    #print(gitshow_cont)
+#                                    pass
+#
+#                            #print(ss_cont[i][:40])
 
             else:
                 if not os.path.exists(s_file):
