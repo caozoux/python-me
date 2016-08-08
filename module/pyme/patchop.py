711830a87055f0364e770af31626b11bf268b7c7
czou <cao.zou@windriver.com>
2016-08-08 10:58:55 +0800

    patch: analysis the patch context to items
    Signed-off-by: czou <cao.zou@windriver.com>

--- module/pyme/patchop.py
+++ module/pyme/patchop.py
@@ -1,9 +1,9 @@
 
 from mfiles import *;
+import os;
 class patchFilter:
     def __init__(self, name):
         self.mPatchName = name
-
 
     @staticmethod
     def getCommit(patchname):
@@ -12,4 +12,20 @@ class patchFilter:
         commitStartLine = re.sub(r'Subject.*]\s',"", ret.mLine)
         commitEndLine=ofile.getLine(ret.mLineNumber+1)
         return commitStartLine + commitEndLine;
+
+
+class patchOperation:
+
+    def __init__(self):
+        pass;
+
+    @staticmethod
+    def patchOpApply(patchname, flag=1):
+        "flag: \
+            0: normal git apply \
+            1: redirectory the error std to std io"
+        if flag == 0:
+            return os.popen("git apply "+patchname).readlines();
+        elif flag == 1:
+            return os.popen("git apply "+patchname+" 2>&1"+" | grep \"error: patch failed:\" ").readlines();
 
