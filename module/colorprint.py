f23db622ba1a80d4d20e0845a38591acd96e3c3b
zou cao <cao.zou@windriver.com>
2016-12-12 17:54:37 +0800

    colorprint: add info, warn and err color print
    Signed-off-by: zou cao <cao.zou@windriver.com>

---
+++ module/colorprint.py
@@ -0,0 +1,20 @@
+def info(dictArg):
+    "green "
+    print '\033[1;32;32m',
+    print dictArg,
+    print '\033[0m'
+    return
+
+def warn(dictArg, color="r"):
+    "y: yellow"
+    print '\033[1;33;33m',
+    print dictArg,
+    print '\033[0m'
+    return
+
+def err(dictArg, color="r"):
+    "r: red"
+    print '\033[1;31;31m',
+    print dictArg,
+    print '\033[0m'
+    return
