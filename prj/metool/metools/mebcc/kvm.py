2afc88e5205f006b50923b5af9f64d1d53f37e29
zou cao <zoucaox@outlook.com>
2022-03-29 10:48:30 +0800

    update

--- prj/metool/metools/mebcc/kvm.py
+++ prj/metool/metools/mebcc/kvm.py
@@ -4,142 +4,142 @@ import argparse
 import signal
 from subprocess import call
 from optparse import OptionParser
+from mebccapi import ArchX86Data
 
 # load BPF program
 kvmbpf_text="""
+
 #include <uapi/linux/ptrace.h>
 #include <linux/blkdev.h>
-char test[]="hi";
-struct key_t {
-	int exit_reason;
+struct kvm_exit_key {
+    int exit_reason;
 };
-BPF_HASH(kvm_record, struct key_t);
+
+struct kvm_msr_key {
+    unsigned int write;
+    u64 data;
+};
+
+BPF_HASH(kvm_exit_hash, struct kvm_exit_key);
+
+BPF_HASH(kvm_msr_hash, struct kvm_msr_key);
+
 struct kvm_entrys_args {
-    u64 __unused__;
-    unsigned int data1;
-    unsigned int data2;
-    unsigned int data3;
+    u64 pid;
+    unsigned int  exit_reason;
+    unsigned long guest_rip;
+    u32  isa;
+    u64 info1;
+    u64 info2;
 };
+
 struct kvm_msr_args {
     u64 __unused__;
     unsigned int write;
-    unsigned int ecx;
-    u64 exception;
+    u32 ecx;
+    u64 data;
+    u8  except;
 };
 
-
-int on_switch(struct kvm_entrys_args *ctx)
+int trace_kvm_exit(struct kvm_entrys_args *ctx)
 {
-    struct key_t key= {};
-    unsigned int exit_reason= (unsigned int)ctx->data1;
-    //bpf_trace_printk("%d %d\\n", exit_reason, exit_reason);
+    struct kvm_exit_key key= {};
+    unsigned int exit_reason= (unsigned int)ctx->exit_reason;
+    bpf_trace_printk("%x %lx\\n", exit_reason, ctx->guest_rip);
     key.exit_reason=exit_reason;
-    kvm_record.increment(key);
+    kvm_exit_hash.increment(key);
     //bpf_trace_printk("zz ");
 }
-int kvm_msr(struct kvm_msr_args *ctx)
+
+int trace_kvm_msr(struct kvm_msr_args *ctx)
 {
-    unsigned int write= (unsigned int)ctx->write;
-    unsigned int ecx= (unsigned int)ctx->ecx;
-    bpf_trace_printk("%d %x\\n", write, ecx);
-    //key.exit_reason=exit_reason;
-    //kvm_record.increment(key);
-    //bpf_trace_printk("zz ");
+    struct kvm_msr_key key={};
+    //unsigned int write= (unsigned int)ctx->write;
+    //unsigned int ecx= (unsigned int)ctx->ecx;
+
+    key.write = ctx->write;
+    key.data = ctx->ecx;
+    kvm_msr_hash.increment(key);
 }
 """
 
-# header
-b = BPF(text=kvmbpf_text)
-#b.attach_tracepoint(tp="kvm:kvm_entry", fn_name="kvm_entry")
-b.attach_tracepoint(tp="kvm:kvm_exit", fn_name="on_switch")
-b.attach_tracepoint(tp="kvm:kvm_msr", fn_name="kvm_msr")
-#b.attach_tracepoint(tp="sched:sched_switch", fn_name="on_switch")
+kvm_trace_event_dict = {
+"kvm:kvm_exit":"trace_kvm_exit",
+"kvm:kvm_msr":"trace_kvm_msr",
+}
 
-kvm_type = [
-"EXIT_REASON_EXCEPTION_NMI",
-"EXIT_REASON_EXTERNAL_INTERRUPT",
-"EXIT_REASON_TRIPLE_FAULT",
-"EXIT_REASON_INIT_SIGNAL",
-"EXIT_INVALID",
-"EXIT_INVALID",
-"EXIT_INVALID",
-"EXIT_REASON_PENDING_INTERRUPT",
-"EXIT_REASON_NMI_WINDOW",
-"EXIT_REASON_TASK_SWITCH",
-"EXIT_REASON_CPUID",
-"EXIT_INVALID",
-"EXIT_REASON_HLT",
-"EXIT_REASON_INVD",
-"EXIT_REASON_INVLPG",
-"EXIT_REASON_RDPMC",
-"EXIT_REASON_RDTSC",
-"EXIT_INVALID",
-"EXIT_REASON_VMCALL",
-"EXIT_REASON_VMCLEAR",
-"EXIT_REASON_VMLAUNCH",
-"EXIT_REASON_VMPTRLD",
-"EXIT_REASON_VMPTRST",
-"EXIT_REASON_VMREAD",
-"EXIT_REASON_VMRESUME",
-"EXIT_REASON_VMWRITE",
-"EXIT_REASON_VMOFF",
-"EXIT_REASON_VMON",
-"EXIT_REASON_CR_ACCESS",
-"EXIT_REASON_DR_ACCESS",
-"EXIT_REASON_IO_INSTRUCTION",
-"EXIT_REASON_MSR_READ",
-"EXIT_REASON_MSR_WRITE",
-"EXIT_REASON_INVALID_STATE",
-"EXIT_REASON_MSR_LOAD_FAIL",
-"EXIT_INVALID",
-"EXIT_REASON_MWAIT_INSTRUCTION",
-"EXIT_REASON_MONITOR_TRAP_FLAG",
-"EXIT_INVALID",
-"EXIT_REASON_MONITOR_INSTRUCTION",
-"EXIT_REASON_PAUSE_INSTRUCTION",
-"EXIT_REASON_MCE_DURING_VMENTRY",
-"EXIT_INVALID",
-"EXIT_REASON_TPR_BELOW_THRESHOLD",
-"EXIT_REASON_APIC_ACCESS",
-"EXIT_REASON_EOI_INDUCED",
-"EXIT_REASON_GDTR_IDTR",
-"EXIT_REASON_LDTR_TR",
-"EXIT_REASON_EPT_VIOLATION",
-"EXIT_REASON_EPT_MISCONFIG",
-"EXIT_REASON_INVEPT",
-"EXIT_REASON_RDTSCP",
-"EXIT_REASON_PREEMPTION_TIMER",
-"EXIT_REASON_INVVPID",
-"EXIT_REASON_WBINVD",
-"EXIT_REASON_XSETBV",
-"EXIT_REASON_APIC_WRITE",
-"EXIT_REASON_RDRAND",
-"EXIT_REASON_INVPCID",
-"EXIT_REASON_VMFUNC",
-"EXIT_REASON_ENCLS",
-"EXIT_REASON_RDSEED",
-"EXIT_REASON_PML_FULL",
-"EXIT_REASON_XSAVES",
-"EXIT_REASON_XRSTORS",
-"EXIT_REASON_UMWAIT",
-"EXIT_REASON_TPAUSE",
-];
-counts = b.get_table("kvm_record") 
+def instance_kvm_exit():
+    counts = b.get_table("kvm_exit_hash") 
 # trace until Ctrl-C
-exiting = 0
-while 1:
-    try:
-        sleep(1)
-    except KeyboardInterrupt:
-        exiting = 1
+    exiting = 0
+    while 1:
+        try:
+            sleep(1)
+        except KeyboardInterrupt:
+            exiting = 1
 
-    print("==================================================")
-    for k, v in counts.items():
-        print("%-40s%-10d"%(kvm_type[k.exit_reason], v.value))
-        #print("%-30d%-10d"%(k.exit_reason, v.value))
+        print("==================================================")
+        for k, v in counts.items():
+            print("%-40s%-10d"%(ArchX86Data.kvm_exit_type1[k.exit_reason], v.value))
+            #print("%-30d%-10d"%(k.exit_reason, v.value))
+
+        if exiting == 1:
+            print("Detaching...")
+            exit()
+
+def instance_kvm_msr():
+    b.attach_tracepoint(tp="kvm:kvm_msr", fn_name="trace_kvm_msr")
+    #kvm_trace_event_dict['kvm:kvm_msr']()
+    counts = b.get_table("kvm_msr_hash") 
+    exiting = 0
+    while 1:
+        try:
+            sleep(1)
+        except KeyboardInterrupt:
+            exiting = 1
+
+        print("==================================================")
+        for k, v in counts.items():
+            if k.data in ArchX86Data.x86_msr_dict.keys():
+                print("%-30s%-10x"%(ArchX86Data.x86_msr_dict[int(k.data)], v.value))
+            else:
+                print("%-30x%-10x"%(k.data, v.value))
+
+        if exiting == 1:
+            print("Detaching...")
+            exit()
+
+kvm_trace_event_dict = {
+"kvm:kvm_exit":instance_kvm_exit,
+"kvm:kvm_msr":instance_kvm_msr,
+}
+
+parser = OptionParser()
+parser.add_option("-l", "--list", action="store_true", dest="list",
+                  help="--list list all benchmark test name") 
+parser.add_option("-r", "--run", type="string", dest="run",
+                  help="--run command")
+(options, args) = parser.parse_args()
 
 
-    if exiting == 1:
-        print("Detaching...")
-        exit()
+def RunCommand(key):
+    if key in kvm_trace_event_dict.keys():
+        kvm_trace_event_dict[key]()
 
+def ListCommand(dictlist):
+    for key in dictlist:
+        print(("%s  %--20s")%("command:", key))
+
+if options.list:
+    ListCommand(kvm_trace_event_dict)
+    exit(0)
+
+if options.run:
+    b = BPF(text=kvmbpf_text)
+    RunCommand(options.run)
+
+# header
+#b.attach_tracepoint(tp="kvm:kvm_exit", fn_name="trace_kvm_exit")
+#b.attach_tracepoint(tp="kvm:kvm_msr", fn_name="trace_kvm_msr")
+#b.attach_tracepoint(tp="sched:sched_switch", fn_name="on_switch")
+kvm_trace_event_dict['kvm:kvm_msr']()
---
+++ prj/metool/metools/mebcc/mestack.py
@@ -0,0 +1,88 @@
+from bcc import BPF
+from bcc.utils import printb
+from time import sleep, strftime
+import argparse
+import signal
+import errno
+from subprocess import call
+from optparse import OptionParser
+from mebccapi import ArchX86Data
+
+bpf_text="""
+#include <uapi/linux/ptrace.h>
+#include <linux/blkdev.h>
+
+struct key_t {
+    int  w_k_stack_id;
+    //char waker[TASK_COMM_LEN];
+    //char target[TASK_COMM_LEN];
+};
+
+BPF_STACK_TRACE(stack_traces, 1024);
+BPF_HASH(counts, struct key_t);
+struct kvm_exit_key {
+    int exit_reason;
+};
+
+int stacktrace(struct pt_regs *ctx, struct task_struct *p) {
+    struct key_t key = {};
+    //u64 delta ;
+
+    //delta = bpf_ktime_get_ns();
+    //delta = delta / 1000;
+    key.w_k_stack_id = stack_traces.get_stackid(ctx, BPF_F_REUSE_STACKID);
+    //counts.increment(key, delta);
+    counts.increment(key);
+    return 0;
+}
+
+"""
+
+def GetFuncList(name):
+    name=name.replace(" ","")
+    name=name.split("+")
+    return name
+
+parser = OptionParser()
+parser.add_option("-l", "--list", action="store_true", dest="list",
+                  help="--list list all benchmark test name") 
+parser.add_option("-f", "--func", type="string", dest="func",
+                  help="--run command")
+
+(options, args) = parser.parse_args()
+
+
+if not options.func:
+    exit(1)
+
+funclist=GetFuncList(options.func)
+b = BPF(text=bpf_text)
+
+for func in funclist:
+    b.attach_kprobe(event=func, fn_name="stacktrace")
+
+#b.attach_kprobe(event="try_to_wake_up", fn_name="stacktrace")
+
+while (1):
+    try:
+        sleep(1)
+    except KeyboardInterrupt:
+        # as cleanup can take many seconds, trap Ctrl-C:
+        signal.signal(signal.SIGINT, signal_ignore)
+    counts = b.get_table("counts")
+    stack_traces = b.get_table("stack_traces")
+    for k, v in sorted(counts.items(), key=lambda counts: counts[1].value):
+        if k.w_k_stack_id == -errno.ENOMEM:
+            missing_stacks += 1
+            continue
+
+        waker_kernel_stack = [] if k.w_k_stack_id < 1 else \
+            reversed(list(stack_traces.walk(k.w_k_stack_id))[1:])
+        # print default multi-line stack output
+        #printb(b"2    %-16s %s" % (b"target:", k.target))
+        for addr in waker_kernel_stack:
+            printb(b"3    %-16x %s" % (addr, b.ksym(addr)))
+        #printb(b"4    %-16s %s" % (b"waker:", k.waker))
+        print("5        %d\n" % v.value)
+    counts.clear()
+    exit()
