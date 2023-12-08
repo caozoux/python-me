1c9e3434489376c94fe5e7598185cfc42c7e7dce
Zou Cao <zoucao@outlook.com>
2023-12-08 00:47:24 -0500

    update

--- prj/bpftrace/unlink.sh
+++ prj/bpftrace/unlink.sh
@@ -2,10 +2,10 @@
 
 #include <linux/fs.h>
 #include <linux/dcache.h>
-kprobe:vfs_rmdir
+kprobe:do_unlinkat
 {
   $d=str(((struct dentry *)arg1)->d_name.name);
   if ( $d == "kess" ) {
   }
- printf("pid:%d comm:%s dir:%s\n", pid, comm, $d);
+  printf("pid:%d comm:%s dir:%s\n", pid, comm, $d);
 }
